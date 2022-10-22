6c21003e87ec1ca49fad22cb119ddee137455a17
vaishnav <vaishnav240204@gmail.com>
2022-10-22 20:35:34 +0530

    satisfying vercel

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,2 @@
-const truncate = (str: any, n: number) => {
-  return str.length > n ? str.substr(0, n - 1) + '...' : str
-}
-
-export { truncate }
+export const truncate = (str: any, n: number) =>
+  str?.length > n ? str.slice(0, n) + '...' : str
--- src/pages/api/weather.ts
+++ src/pages/api/weather.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=chennai&units=metric&appid=${process.env.WEATHER_API_KEY}`,
