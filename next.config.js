a6d4b7788c893e2b6e9de93c387c6e5073a07d66
vaishnav <84540554+vaishnav-mk@users.noreply.github.com>
2024-03-09 06:36:03 +0000

    resume update

--- next.config.js
+++ next.config.js
@@ -43,7 +43,7 @@ module.exports = {
       {
         source: '/view-resume',
         destination:
-          'https://drive.google.com/file/d/1X-6ZBfiRPC9WZx3f9Pih8gE49WQRoQ9X/view?usp=sharing',
+          'https://drive.google.com/file/d/1UJ-wExAAq4eZbCbw2TwrrVvAHMM5uP3a/view?usp=sharing',
         permanent: true,
       },
       {
