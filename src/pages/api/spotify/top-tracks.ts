05044ecc234cb3eb8b7aecf9d758d7cf4489e76b
vaishnav <84540554+vaishnav-mk@users.noreply.github.com>
2024-09-15 13:18:57 +0000

    remove top tracks

--- src/pages/api/spotify/top-tracks.ts
+++ src/pages/api/spotify/top-tracks.ts
@@ -2,18 +2,6 @@ import { getTopTracks } from 'lib/spotify'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (_: any, res: any) => {
-  const response = await getTopTracks()
-  const { items } = await response.json()
 
-  const tracks = items.slice(0, 10).map((track: any) => ({
-    artist: track.artists.map((_artist: any) => _artist.name),
-    songUrl: track.external_urls.spotify,
-    title: track.name,
-    images: track.album.images.map((image: any) => image.url),
-    type: track.type,
-    releaseDate: track.album.release_date,
-    duration: track.duration_ms,
-  }))
-
-  return res.status(200).json(tracks)
+  return res.status(200).json([])
 }
