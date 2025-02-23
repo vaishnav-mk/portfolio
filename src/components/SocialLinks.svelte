375ab30d223301fe088bde8fc733b8a2c4833a37
vaishnav-mk <vaishnav240204@gmail.com>
2025-02-23 23:18:43 +0530

    bl

--- src/components/SocialLinks.svelte
+++ src/components/SocialLinks.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <div class="relative md:mt-16">
-  <div
-    class="relative z-10 flex flex-row items-center gap-4 sm:gap-6 flex-wrap"
-  >
-    <span class="text-sunrise font-bold">~/vm:</span>
+  <div class="relative z-10 flex flex-row items-center gap-4 sm:gap-6 flex-wrap">
+    <a href="/bl" class="text-sunrise font-bold">
+      ~/vm:
+    </a>
     {#each links as link}
       {#if link.label === "Twitter"}
         <a
